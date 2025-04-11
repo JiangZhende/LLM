83671c0c7a7f19e8d80690eaf03154d9c6162ccc
JiangZhende <1303720427@qq.com>
2025-04-11 22:22:18 +0800

    数据集下载

--- LLM/prepare.sh
+++ LLM/prepare.sh
@@ -1,1 +1,6 @@
+export HF_ENDPOINT=https://hf-mirror.com
 huggingface-cli download --repo-type dataset pleisto/wikipedia-cn-20230720-filtered --local-dir pleisto/wikipedia-cn-20230720-filtered
+# huggingface-cli download --repo-type dataset xuqinyang/BaiduBaike-5.63M --local-dir xuqinyang/BaiduBaike-5.63M
+# huggingface-cli download --repo-type dataset wangrui6/Zhihu-KOL --local-dir wangrui6/Zhihu-KOL
+# huggingface-cli download --repo-type dataset wdndev/webnovel-chinese --local-dir wdndev/webnovel-chinese
+# huggingface-cli download --repo-type dataset TigerResearch/pretrain_zh --local-dir TigerResearch/pretrain_zh
