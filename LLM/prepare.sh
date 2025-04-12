5576eb01155d085bcea841a86f5904078ea79d62
JiangZhende <1303720427@qq.com>
2025-04-12 23:11:23 +0800

    数据下载

--- LLM/prepare.sh
+++ LLM/prepare.sh
@@ -1,6 +1,6 @@
 export HF_ENDPOINT=https://hf-mirror.com
-huggingface-cli download --repo-type dataset pleisto/wikipedia-cn-20230720-filtered --local-dir pleisto/wikipedia-cn-20230720-filtered
-# huggingface-cli download --repo-type dataset xuqinyang/BaiduBaike-5.63M --local-dir xuqinyang/BaiduBaike-5.63M
-# huggingface-cli download --repo-type dataset wangrui6/Zhihu-KOL --local-dir wangrui6/Zhihu-KOL
-# huggingface-cli download --repo-type dataset wdndev/webnovel-chinese --local-dir wdndev/webnovel-chinese
-# huggingface-cli download --repo-type dataset TigerResearch/pretrain_zh --local-dir TigerResearch/pretrain_zh
+huggingface-cli download --repo-type dataset pleisto/wikipedia-cn-20230720-filtered --local-dir datasets/pleisto/wikipedia-cn-20230720-filtered
+huggingface-cli download --repo-type dataset xuqinyang/BaiduBaike-5.63M --local-dir datasets/xuqinyang/BaiduBaike-5.63M
+huggingface-cli download --repo-type dataset wangrui6/Zhihu-KOL --local-dir datasets/wangrui6/Zhihu-KOL
+huggingface-cli download --repo-type dataset wdndev/webnovel-chinese --local-dir datasets/wdndev/webnovel-chinese
+huggingface-cli download --repo-type dataset TigerResearch/pretrain_zh --local-dir datasets/TigerResearch/pretrain_zh
