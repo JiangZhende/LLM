72ec7f748d0db8f51547664494c0d412578143a9
JiangZhende <1303720427@qq.com>
2025-09-10 22:12:13 +0800

    sft 增加plog loss

--- train/sft_train.py
+++ train/sft_train.py
@@ -4,6 +4,7 @@ from typing import Optional
 import sys
 import os
 
+from matplotlib import pyplot as plt
 import torch
 import transformers
 from transformers import (
@@ -209,6 +210,26 @@ def main():
     # os.makedirs(last_model_dir, exist_ok=True)
     # tokenizer.save_pretrained(last_model_dir)
     # trainer.save_model(last_model_dir)
+    def plot_loss(save_directory, log_history):
+        plt.switch_backend("agg")
+        key = "loss"
+        steps, metrics = [], []
+        for log_entry in log_history:
+            if key in log_entry:
+                steps.append(log_entry["step"])
+                metrics.append(log_entry[key])
+
+        plt.figure()
+        plt.plot(steps, metrics, color="#1f77b4", label="loss")
+        plt.title(f"Training {key} Curve")
+        plt.xlabel("Step")
+        plt.ylabel(key.capitalize())
+        plt.legend()
+        figure_path = os.path.join(save_directory, f"training_{key.replace('/', '_')}.png")
+        plt.savefig(figure_path, format="png", dpi=100)
+        print(f"Loss curve saved to {figure_path}")
+
+    plot_loss(training_args.output_dir, trainer.state.log_history)
 
 if __name__ == "__main__":
     main()
